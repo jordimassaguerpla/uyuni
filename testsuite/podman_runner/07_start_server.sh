946cc9a885ddc997ce98990452aae85667e206c2
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2024-05-31 10:57:57 +0200

    use a different port
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.com>

--- testsuite/podman_runner/07_start_server.sh
+++ testsuite/podman_runner/07_start_server.sh
@@ -24,6 +24,7 @@ sudo -i podman run --cap-add AUDIT_CONTROL --rm \
     -h server \
     -p 8443:443 \
     -p 8080:80 \
+    -p 9090:9090 \
     -p 4505:4505 \
     -p 4506:4506 \
     -d --name=server \
