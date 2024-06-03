e1e6a7decaf28a9d6832c3f39605a9329ee3884a
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2024-06-03 10:38:46 +0200

    fix
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.com>

--- testsuite/podman_runner/14_run_salt_rhlike_minion.sh
+++ testsuite/podman_runner/14_run_salt_rhlike_minion.sh
@@ -11,5 +11,5 @@ sudo -i podman exec rhlike_minion bash -c "ssh-keygen -A && /usr/sbin/sshd -e"
 sudo -i podman exec rhlike_minion bash -c "if [ ! -d /root/.ssh ];then mkdir /root/.ssh/;chmod 700 /root/.ssh;fi;cp /tmp/authorized_keys /root/.ssh/"
 # sudo -i podman exec -d rhlike_minion prometheus
 sudo -i podman exec -d rhlike_minion node_exporter
-sudo -i podman exec -d prometheus-apache_exporter
-sudo -i podman exec -d -e DATA_SOURCE_NAME="postgresql://user:passwd@localhost:5432/database?sslmode=disable" prometheus-postgres_exporter
+sudo -i podman exec -d rhlike_minion prometheus-apache_exporter
+sudo -i podman exec -d -e DATA_SOURCE_NAME="postgresql://user:passwd@localhost:5432/database?sslmode=disable" rhlike_minion prometheus-postgres_exporter
